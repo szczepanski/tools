af79dd4f81133be50e6a4dcf04e43985d0c96d82
Piotr <ask.piotr@yahoo.co.uk>
2019-11-12 16:54:01 +0100

    Update yaml_parser.py

--- yaml-parser/yaml_parser.py
+++ yaml-parser/yaml_parser.py
@@ -1,1 +1,25 @@
-yaml_parser.py
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ptr.szczepanski@gmail.com
+
+import sys, yaml, pprint
+
+
+# variables
+file_path = sys.argv[1]
+
+
+# functions
+def readYaml(file_path):
+    pprint.pprint(yaml.load(open(file_path).read()))
+
+
+# main
+def main():
+    readYaml(file_path)
+
+  
+# run main
+if __name__ == '__main__':
+    main()
