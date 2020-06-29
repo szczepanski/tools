0288b90c450d71550661b34a05d4e3031d84f770
Piotr <ask.piotr@yahoo.co.uk>
2020-06-29 11:53:53 +0200

    Update pdf-merger.py

--- pdf-merger/pdf-merger.py
+++ pdf-merger/pdf-merger.py
@@ -1,11 +1,40 @@
+#!/usr/bin/env python3
+"""
+simple pdf merger 
+sample usage to merge 27 pdf files ['1.pdf', '2.pdf' ..., '27.pdf']:
+
+pdfmerger.py 27
+
+"""
+
+__author__ = "Piotr Szczepanski"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
 from PyPDF2 import PdfFileMerger
+import sys
 
-pdfs = ['1.pdf', '2.pdf', '3.pdf', '4.pdf', '5.pdf', '6.pdf', '7.pdf', '8.pdf', '9.pdf', '10.pdf', '11.pdf', '.pdf', '.pdf', '.pdf', '.pdf', '.pdf', '.pdf', '.pdf', '.pdf', '.pdf', '.pdf', '.pdf', '.pdf', '.pdf', '.pdf', '.pdf', '.pdf']
+number_of_files = int(sys.argv[1])
 
-merger = PdfFileMerger()
+def merge_pdfs(number_of_files):
+    pdfs = []
+    for index in range(0, number_of_files):
+        pdfs.append('{file_number}.pdf'.format(file_number = index + 1))
 
-for pdf in pdfs:
-    merger.append(pdf)
 
-merger.write("merged.pdf")
-merger.close()
+    merger = PdfFileMerger()
+
+    for pdf in pdfs:
+        merger.append(pdf)
+
+    merger.write('merged.pdf')
+    merger.close()
+
+
+def main(number_of_files):
+    merge_pdfs(number_of_files)
+
+
+if __name__ == "__main__":
+    main(number_of_files)
